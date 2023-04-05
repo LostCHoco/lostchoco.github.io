9904ab583ab441274ce1114dcc61f64f5b4032b0
Pengdoll-LCH <58216826+Pengdoll-LCH@users.noreply.github.com>
2023-04-06 07:45:13 +0900

    .

--- pixel.js
+++ pixel.js
@@ -1,24 +1,22 @@
-const Wrapper={
-    width:window.innerWidth,
-    height:window.innerHeight,
-    setSize:(x,y)=>{
-        Wrapper.width=x;
-        Wrapper.height=y;
-        const wrapper=document.querySelector("#wrapper");
-        wrapper.style.width=Wrapper.width+"px";
-        wrapper.style.height=Wrapper.height+"px";
-        wrapper.style.gridTemplateRows="repeat("+Wrapper.height/10+",1fr)";
-        wrapper.style.gridTemplateColumns="repeat("+Wrapper.width/10+",1fr)";
+const You={
+    x:0,
+    y:0,
+    color:"white",
+    set:()=>{
+
     },
-    remake:()=>{
-        const wrapper=document.querySelector("#wrapper");
-        wrapper.remove();
-        const newWrapper=document.createElement("section");
-        newWrapper.id="wrapper";
-        document.body.appendChild(newWrapper);
+    move:()=>{
+
     }
 }
-document.body.style.gridTemplateColumns
+const Pixel={
+    x:document.querySelector(".pixel").clientWidth,
+    y:document.querySelector(".pixel").clientHeight
+}
+const Wrapper={
+    x:document.querySelector("#wrapper").clientWidth,
+    y:document.querySelector("#wrapper").clientHeight
+}
 function createPixel(){
     const newPixel=document.createElement("div");
     newPixel.classList.add("pixel");
@@ -26,13 +24,8 @@ function createPixel(){
     wrapper.appendChild(newPixel);
 }
 window.addEventListener("load",()=>{
-    for(let i=0;i<60*80;i++){
+    for(let i=0;i<40*60-1;i++){
         createPixel();
     }
 })
-window.addEventListener("resize",(x,y)=>{
-    x=window.innerWidth-window.innerWidth%10;
-    y=window.innerHeight-window.innerHeight%10;
-    Wrapper.remake();
-    Wrapper.setSize(x,y);
-})
+console.log(Wrapper.y/Pixel.y);
---
+++ test.js
@@ -0,0 +1,20 @@
+window.onload=()=>{
+    let x=document.querySelector("body");
+    console.log(x);
+}
+function createDiv(){
+    let newDiv=document.createElement("div");
+    document.body.appendChild(newDiv);
+}
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
+createDiv();createDiv();createDiv();createDiv();createDiv();
